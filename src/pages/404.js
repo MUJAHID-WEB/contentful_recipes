c87461c83a12b5e0eaf4a4b3961d1dd3de350847
MUJAHID-WEB <mujahid30390@gmail.com>
2024-03-14 15:54:02 +0600

    404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -15,7 +15,7 @@ const NotFound = () => {
     <div className="not-found">
       <h1>404</h1>
       <h2>Oops! That page cannot be found :(</h2>
-      <p>Redirecting to <Link href="/"><a>Homepage</a></Link> for more marmite goodness...</p>
+      <p>Redirecting to <Link href="/">Homepage</Link> for more ...</p>
 
       <style jsx>{`
         .not-found {
